28b2925ac52a3648b6d914648d492c42a15cb145
Vanya U <ivict@rambler.ru>
2023-02-05 19:16:43 +0300

    # There is the few drops from async approach (useLazyQuery + state instead useQuery).

--- src/components/reports/TransactionReportBody.tsx
+++ src/components/reports/TransactionReportBody.tsx
@@ -1,12 +1,15 @@
-import React, { useEffect, useState } from "react";
-import { useNavigate, useSearchParams } from "react-router-dom";
-import { GET_TRANSACTIONS } from '../../apollo/get-transactions';
-import DataTable from '../data-driven/DataTable';
+import { useLazyQuery } from '@apollo/client'
+import React, { useCallback, useEffect, useState } from 'react';
+import { GET_TRANSACTIONS } from '../../apollo/get-transactions'
 import { TransactionReportFilter } from '../../filters/TransactionReportFilter'
-import { Transaction } from '../../model/Transaction';
-import data from '../../data/transactions.json'
-import { userColumnDefs as transactionColumnDefs } from '../data-driven/TransactionColumnDefs';
-import { useQuery } from "@apollo/client";
+import { Transaction } from '../../model/Transaction'
+import DataTable from '../data-driven/DataTable'
+import { userColumnDefs as transactionColumnDefs } from '../data-driven/TransactionColumnDefs'
+
+
+export interface TransactionReportBodyState {
+    filteredData: Transaction[]
+}
 
 
 export interface TransactionReportBodyProps {
@@ -14,16 +17,32 @@ export interface TransactionReportBodyProps {
     filter?: TransactionReportFilter
 }
 
+
 const TransactionTable = DataTable<Transaction>()
+
 
 export const TransactionReportBody: React.FC<TransactionReportBodyProps> = ({ className = '', filter }) => {
 
+    const [state, setValue] = useState<TransactionReportBodyState>({
+        filteredData: []
+    })
+
     console.debug(`Filter is ${JSON.stringify(filter)}`)
-    const { data, loading, error } = useQuery(GET_TRANSACTIONS(filter))
-    const filteredData = data ? data.transactionsRelayConnection.edges.map((node: any) => node.node as Transaction) : []
-//HERE <<<<<<<<<<<
+    const [getRequest, { loading, error, data, refetch, networkStatus }] = useLazyQuery(GET_TRANSACTIONS(filter))
+
+    const fetchData = useCallback(async () => {
+        getRequest().then((result) => {
+            setValue({ filteredData: result.data.transactionsRelayConnection.edges.map((node: any) => node.node as Transaction) })
+        })
+    }, [getRequest])
+
+    useEffect(() => {
+        fetchData().catch(console.error)
+    }, [fetchData])
+
     return (
-    <div className={className}>
-        <TransactionTable data={filteredData} columnDefs={transactionColumnDefs}/>
-    </div>
-)}
+        <div className={className}>
+            <TransactionTable data={state.filteredData} columnDefs={transactionColumnDefs} />
+        </div>
+    )
+}
